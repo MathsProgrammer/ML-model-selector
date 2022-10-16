5b2b73da9a24b37bc044226aa4c05e3c20fe326a
MathsProgrammer <92876061+MathsProgrammer@users.noreply.github.com>
2022-10-16 13:50:30 +0100

    Update code.py

--- code.py
+++ code.py
@@ -8,6 +8,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from xgboost import XGBClassifier
 from sklearn.metrics import f1_score,precision_score,accuracy_score,recall_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn import svm
+from sklearn.neural_network import MLPClassifier
+
 
 #Read and show data
 
@@ -60,13 +64,27 @@ xgb.fit(X_train , y_train)
 xgb.score(X_test , y_test).round(5)
 xgb_preds=xgb.predict(X_test)
 
+#AdaBoost
+
+ada = AdaBoostClassifier()
+ada.fit(X_train , y_train)
+ada.score(X_test , y_test).round(5)
+ada_preds=ada.predict(X_test)
+
 
 #Need to scale the data for the following methods
 
 scale = StandardScaler()
 X_train, X_test = scale.fit_transform(X_train),scale.fit_transform(X_test)
     
-    
+#Support vector machines
+
+svm = svm.SVC()    
+svm.fit(X_train , y_train)
+svm.score(X_train , y_train).round(5)
+svm_preds = svm.predict(X_test)
+
+
 #Logistic regression
 
 lr = LogisticRegression(C = 100)
@@ -81,18 +99,30 @@ knn.fit(X_train, y_train)
 knn.score(X_test, y_test).round(5)
 knn_preds = knn.predict(X_test)
 
+#Neural networks
+
+net = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+net.fit(X_train, y_train)
+net.score(X_test, y_test).round(5)
+net_preds = net.predict(X_test)
+
+
 #Results
 
 model_comparison={}
 model_comparison['Decision Tree']=[accuracy_score(y_test,DT_preds),f1_score(y_test,DT_preds,average='weighted'),precision_score(y_test,DT_preds),recall_score(y_test,DT_preds)]
-model_comparison['Naive']=[accuracy_score(y_test,naive_preds),f1_score(y_test,naive_preds,average='weighted'),precision_score(y_test,naive_preds),recall_score(y_test,naive_preds)]                                   
+model_comparison['Naive Bayes']=[accuracy_score(y_test,naive_preds),f1_score(y_test,naive_preds,average='weighted'),precision_score(y_test,naive_preds),recall_score(y_test,naive_preds)]                                   
 model_comparison['Random forest']=[accuracy_score(y_test,Rclf_preds),f1_score(y_test,Rclf_preds,average='weighted'),precision_score(y_test,Rclf_preds),recall_score(y_test,Rclf_preds)]
 model_comparison['KNN']=[accuracy_score(y_test,knn_preds),f1_score(y_test,knn_preds,average='weighted'),precision_score(y_test,knn_preds),recall_score(y_test,knn_preds)] 
 model_comparison['XGboost']=[accuracy_score(y_test,xgb_preds),f1_score(y_test,xgb_preds,average='weighted'),precision_score(y_test,xgb_preds),recall_score(y_test,xgb_preds)]
-model_comparison['Logisticregerssion']=[accuracy_score(y_test,LR_preds),f1_score(y_test,LR_preds,average='weighted'),precision_score(y_test,LR_preds),recall_score(y_test,LR_preds)] 
+model_comparison['Logistic Regerssion']=[accuracy_score(y_test,LR_preds),f1_score(y_test,LR_preds,average='weighted'),precision_score(y_test,LR_preds),recall_score(y_test,LR_preds)] 
+model_comparison['Adaboost']=[accuracy_score(y_test,ada_preds),f1_score(y_test,ada_preds,average='weighted'),precision_score(y_test,ada_preds),recall_score(y_test,ada_preds)]
+model_comparison['SVM']=[accuracy_score(y_test,svm_preds),f1_score(y_test,svm_preds,average='weighted'),precision_score(y_test,svm_preds),recall_score(y_test,svm_preds)]
+model_comparison['Neural networks']=[accuracy_score(y_test,net_preds),f1_score(y_test,net_preds,average='weighted'),precision_score(y_test,net_preds),recall_score(y_test,net_preds)]
+
 
 df = pd.DataFrame(model_comparison).T
 df.columns=['Model Accuracy','Model F1-Score','precision','recall']
-df = df.sort_values(by='Model F1-Score',ascending=False)
+df = df.sort_values(by='Model Accuracy',ascending=False)
 
 print(df)
